48fe909b3cfbeb0ce48591572d5425df0de28ff8
Henok Dereje <henzdereje@gmail.com>
2026-04-29 04:42:46 -0700

    i lifted the state up to the app component because i can use the state information which is the object not only for the userInput but also for the coming component which helps me to show the data as a table

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,23 +1,5 @@
-import { useState } from "react";
-
-
-
-export default function UserInput() {
-    const[userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10,
-    });
-
-    function handleChange(inputIdentifier, newValue){
-        setUserInput((prevUserInput)=>{
-            return{
-                ...prevUserInput,
-                [inputIdentifier]: +newValue
-            }
-        })
-    }
+export default function UserInput({userInput, onChange}) {
+   
   return (
     <section id="user-input">
         <div className="input-group">
